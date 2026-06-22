465fa6a00e8295e6d6387f21a633e9bf28479827
Linko-zhao <1258608694@qq.com>
2026-06-22 08:56:25 +0800

    change md preview plugin

--- lua/plugins/mdpreview.lua
+++ lua/plugins/mdpreview.lua
@@ -1,9 +1,18 @@
+-- return {
+-- 	{
+-- 		'MeanderingProgrammer/render-markdown.nvim',
+-- 		dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' }, -- if you prefer nvim-web-devicons
+-- 		---@module 'render-markdown'
+-- 		---@type render.md.UserConfig
+-- 		opts = {},
+-- 	}
+-- }
 return {
-	{
-		'MeanderingProgrammer/render-markdown.nvim',
-		dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' }, -- if you prefer nvim-web-devicons
-		---@module 'render-markdown'
-		---@type render.md.UserConfig
-		opts = {},
-	}
+    "OXY2DEV/markview.nvim",
+    lazy = false,
+
+    -- Completion for `blink.cmp`
+    -- dependencies = { "saghen/blink.cmp" },
+		opts = {
+		},
 }
