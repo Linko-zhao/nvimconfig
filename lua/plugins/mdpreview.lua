8a43c43e41f9fded18b7071c0e775540e025ddf4
Linko-zhao <1258608694@qq.com>
2026-09-01 14:35:09 +0800

    feat: change markdown preview tool

--- lua/plugins/mdpreview.lua
+++ lua/plugins/mdpreview.lua
@@ -7,12 +7,27 @@
 -- 		opts = {},
 -- 	}
 -- }
-return {
-    "OXY2DEV/markview.nvim",
-    lazy = false,
+-- return {
+--     "OXY2DEV/markview.nvim",
+--     lazy = false,
+--
+--     -- Completion for `blink.cmp`
+--     -- dependencies = { "saghen/blink.cmp" },
+-- 		opts = {
+-- 		},
+-- }
 
-    -- Completion for `blink.cmp`
-    -- dependencies = { "saghen/blink.cmp" },
-		opts = {
-		},
+return {
+	"selimacerbas/markdown-preview.nvim",
+  dependencies = { "selimacerbas/live-server.nvim" },
+  config = function()
+    require("markdown_preview").setup({
+      -- all optional; sane defaults shown
+      instance_mode = "takeover",  -- "takeover" (one tab) or "multi" (tab per instance)
+      port = 0,                    -- 0 = auto (8421 for takeover, OS-assigned for multi)
+      open_browser = true,
+      default_theme = "dark",      -- "dark" or "light"; initial preview theme
+      debounce_ms = 300,
+    })
+  end,
 }
